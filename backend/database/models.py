b1c0cef19e9ffe16d6133d54435e23902a29ea7e
Егор <ohaeohae6732gmail.com>
2025-04-21 21:59:56 +0300

    add model chats

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, INTEGER, String
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column
+from sqlalchemy import create_engine, INTEGER, String, JSON, ForeignKey
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship 
 
 engine = create_engine("sqlite:///backend/database/mydb.db")
 
@@ -12,7 +12,14 @@ class TableUsers(Base):
     name: Mapped[str]
     surname: Mapped[str]
     password: Mapped[str]
+    chats = relationship("TableChats", back_populates="users")
 
+class TableChats(Base):
+    __tablename__ = "chats"
+    id: Mapped[int] = mapped_column(INTEGER, primary_key=True, autoincrement=True)
+    messages: Mapped[str] = mapped_column(JSON)
+    users = relationship("TableUsers", back_populates="chats")
+    user_id: Mapped[int] = mapped_column(INTEGER, ForeignKey("users.id"))
 
 def create_db():
     Base.metadata.create_all(bind=engine)
--- backend/jwt.py
+++ backend/jwt.py
@@ -7,5 +7,6 @@ config.JWT_TOKEN_LOCATION = ["cookies"]
 config.JWT_SECRET_KEY = "secret"
 config.JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
 config.JWT_COOKIE_CSRF_PROTECT = False
+config.JWT_ALGORITHM = "HS256"
 
 security = AuthX(config=config)
